56214c3b681921e848bd3da6be7d70b6b8a44cdc
shelby70 <ajm.istiaque@g.bracu.ac.bd>
2024-09-18 12:00:18 +0600

    buttons are fixed

--- js/department.js
+++ js/department.js
@@ -4,9 +4,13 @@ const loadData = async () => {
   console.log("hey");
   const response = await fetch("fake_department.json");
   const data = await response.json();
-  const container = document.getElementById("main-button");
+  const container = document.getElementById("button-group");
   data.forEach((item) => {
-    const new_button = `<button class="btn">${item}</buttongi>`;
+    const new_button = `
+    <div class= "button-container"> 
+      <button class="btn">${item}</button>
+    </div>
+    `;
     container.innerHTML += new_button;
   });
 };
