b081e3b8d7ae23ccdfd407c82b2c841b52468752
AuroraGrace501 <ttasfia279@gmail.com>
2024-09-18 15:53:31 +0600

    department active buttons functionality done

--- js/department.js
+++ js/department.js
@@ -1,18 +1,32 @@
-let active = "button_container1";
-let active_id = "1000";
-const loadData = async () => {
-  console.log("hey");
+let active = "btn0";
+const load_button = async () => {
   const response = await fetch("fake_department.json");
   const data = await response.json();
   const container = document.getElementById("button-group");
-  data.forEach((item) => {
+  data.forEach((item, id) => {
     const new_button = `
-    <div class= "button-container"> 
-      <button class="department-btn">${item}</button>
+    <div id="btn${id}" onclick="button_changer(this)">
+      <button  class="department-btn ${id ? "" : "active-btn"}">${item}</button>
     </div>
     `;
     container.innerHTML += new_button;
   });
 };
 
-loadData();
+const button_changer = (data) => {
+  let button_to_inactive = document.getElementById(active);
+  text = button_to_inactive.textContent;
+  button_to_inactive.innerHTML = `
+  <button id="btn${active}" class="department-btn">${text}</button>
+  `;
+  console.log(button_to_inactive.innerHTML);
+
+  active = data.id;
+  let button_to_active = document.getElementById(active);
+  text = button_to_active.textContent;
+  button_to_active.innerHTML = `
+      <button id="btn${active}" class="department-btn active-btn">${text}</button>
+  `;
+};
+
+load_button();
