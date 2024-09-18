fcf73777ac9151f61a895eab1967b595ab0c669c
AuroraGrace501 <ttasfia279@gmail.com>
2024-09-18 17:18:50 +0600

    doctor cards dynamic done

--- js/department.js
+++ js/department.js
@@ -19,14 +19,93 @@ const button_changer = (data) => {
   button_to_inactive.innerHTML = `
   <button id="btn${active}" class="department-btn">${text}</button>
   `;
-  console.log(button_to_inactive.innerHTML);
 
   active = data.id;
   let button_to_active = document.getElementById(active);
-  text = button_to_active.textContent;
+  text = button_to_active.textContent.trim();
   button_to_active.innerHTML = `
       <button id="btn${active}" class="department-btn active-btn">${text}</button>
   `;
+  department_doctor(text);
+};
+
+function make_star() {
+  const p = document.createElement("p");
+  p.textContent = `Rating: ${rating}`;
+  return p;
+}
+
+const department_doctor = async (department = "All") => {
+  console.log(department);
+  const response = await fetch("fake_doctor.json");
+  const data = await response.json();
+  const container = document.getElementById("doctor-info");
+  container.innerHTML = "";
+  data.forEach((item, id) => {
+    console.log(
+      department,
+      item.specialization,
+      department == item.specialization,
+      typeof department,
+      typeof item.specialization,
+      department.length,
+      item.specialization.length
+    );
+    if (department == "All" || department == item.specialization) {
+      const doctor_card = `
+      <div id="container">
+           <div class="doctor-image">
+             <img src="${item.image}" alt="" />
+           </div>
+ 
+           <div class="doctor-details">
+             <h4>${item.name}</h4>
+ 
+             <span id ="doc${id}" class="hint-star star">
+             </span>
+ 
+             <div class="info">
+               <ul>
+                 <li><strong>Specialization : </strong>${item.specialization}</li>
+                 <li><strong>Phone : </strong>${item.phone}</li>
+                 <li><strong>Location: </strong>${item.location}</li>
+                 <li><strong>Availability: </strong>${item.availability}</li>
+    
+               </ul>
+             </div>
+ 
+             <div class="control">
+               <button class="btn">
+                 <span class="price">$${item.fee}</span>
+                 <span class="shopping-cart"
+                   ><i class="fa fa-bookmark" aria-hidden="true"></i
+                 ></span>
+                 <span class="buy">Book Now</span>
+               </button>
+             </div>
+           </div>
+         </div>
+     
+     `;
+      container.innerHTML += doctor_card;
+      const star_container = document.getElementById(`doc${id}`);
+      for (let i = 1; i <= 5; i++) {
+        if (i <= Math.floor(item.rating)) {
+          star_container.innerHTML += `
+         <i class="fa fa-star" aria-hidden="true"></i>
+ `;
+        } else if (i <= Math.ceil(item.rating)) {
+          star_container.innerHTML += `
+           <i class="fa fa-star-half-o" aria-hidden="true"></i>
+   `;
+        } else
+          star_container.innerHTML += `
+                   <i class="fa fa-star-o" aria-hidden="true"></i>
+ `;
+      }
+    }
+  });
 };
 
 load_button();
+department_doctor();
